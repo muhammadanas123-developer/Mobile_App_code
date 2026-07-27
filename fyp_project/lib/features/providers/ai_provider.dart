d12f9266b78bb5aa51a3976b270e3053054895fe
Muhammad Anas <muhanmadanas8@gmail.com>
2026-07-26 23:24:59 -0700

    Update ai_provider.dart

--- fyp_project/lib/features/providers/ai_provider.dart
+++ fyp_project/lib/features/providers/ai_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../data/ai_service.dart';
+import '../data/ai_api_service.dart';
 
-final aiServiceProvider =
-Provider<AIService>((ref) {
-  return AIService();
+/// Provider exposing AIApiService connected to FastAPI backend
+final aiServiceProvider = Provider<AIApiService>((ref) {
+  return ref.watch(aiApiServiceProvider);
 });
